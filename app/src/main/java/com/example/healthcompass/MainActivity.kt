bfc2daf18894a7125d468aaf018cf6c0ac832775
yii-meow <yiksoonchong2002@gmail.com>
2024-05-02 00:46:53 +0800

    update summary page

--- app/src/main/java/com/example/healthcompass/MainActivity.kt
+++ app/src/main/java/com/example/healthcompass/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.healthcompass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.summary)
+
+        var tvDate: TextView = findViewById(R.id.tvDate)
+        tvDate.text = "123"
+
+        // Get the current date
+        val currentDate = Date()
+
+        // Define a date format
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+        // Format the current date
+        val formattedDate = dateFormat.format(currentDate)
+
+        // Set the formatted date to the TextView
+        tvDate.text = formattedDate
     }
 }

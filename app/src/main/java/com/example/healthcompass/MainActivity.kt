4e47d81d3e78676a62f55c166a2d710ddd60b72d
yii-meow <yiksoonchong2002@gmail.com>
2024-05-09 21:50:28 +0800

    workout timer

--- app/src/main/java/com/example/healthcompass/MainActivity.kt
+++ app/src/main/java/com/example/healthcompass/MainActivity.kt
@@ -12,14 +12,18 @@ import java.util.Locale
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.nutrition)
+        setContentView(R.layout.fitness_routines)
 
-        val btnEditGoal: Button = findViewById(R.id.btnEditGoal)
+//        val btnEditGoal: Button = findViewById(R.id.btnEditGoal)
+//
+//        btnEditGoal.setOnClickListener {
+//            val intent = Intent(this,edit_goal::class.java)
+//            startActivity(intent)
+//        }
 
-        btnEditGoal.setOnClickListener {
-            val intent = Intent(this,edit_goal::class.java)
-            startActivity(intent)
-        }
+
+
+
 
 //        var tvDate: TextView = findViewById(R.id.tvDate)
 //        tvDate.text = "123"

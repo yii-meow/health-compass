8983ef4e9a89e551dfd036b11f26d8d87bccfe9c
yiyi_meow <80099417+yii-meow@users.noreply.github.com>
2024-05-15 11:37:55 +0800

    firebase configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+<<<<<<< Updated upstream
     id("androidx.navigation.safeargs")
+=======
+    id("com.google.gms.google-services")
+>>>>>>> Stashed changes
 }
 
 android {
@@ -46,6 +50,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.google.firebase:firebase-database:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

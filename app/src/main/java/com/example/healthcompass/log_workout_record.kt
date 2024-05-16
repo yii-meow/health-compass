0b757ef5db341e9a3f6dda371f98c369147609f9
yii-meow <yiksoonchong2002@gmail.com>
2024-05-16 12:18:48 +0800

    log workout

--- app/src/main/java/com/example/healthcompass/log_workout_record.kt
+++ app/src/main/java/com/example/healthcompass/log_workout_record.kt
@@ -1,14 +1,19 @@
 package com.example.healthcompass
 
+import android.app.TimePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
+import android.widget.TimePicker
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import java.util.Calendar
+
 
 class log_workout_record : Fragment() {
     override fun onCreateView(
@@ -29,6 +34,27 @@ class log_workout_record : Fragment() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
 
         fitnessActivitiesSpinner.adapter = adapter
+
+        val tvDurationPicker: TextView = view.findViewById(R.id.durationPicker)
+
+        tvDurationPicker.setOnClickListener {
+            val c = Calendar.getInstance()
+
+            val hour = c.get(Calendar.HOUR_OF_DAY)
+            val minute = c.get(Calendar.MINUTE)
+
+            val timePickerDialog = TimePickerDialog(
+                requireContext(),
+                { view, hourOfDay, minute ->
+                    tvDurationPicker.setText("$hourOfDay:$minute")
+                },
+                hour,
+                minute,
+                false
+            )
+            timePickerDialog.show()
+        }
+
 
         view.findViewById<Button>(R.id.btnBack).setOnClickListener {
             findNavController().popBackStack()

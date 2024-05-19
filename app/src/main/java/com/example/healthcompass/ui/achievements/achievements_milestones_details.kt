993d9d1e4a563620e8cebf8bad69c45d0aa78f4e
yiyi_meow <80099417+yii-meow@users.noreply.github.com>
2024-05-19 22:36:51 +0800

    download badge image

--- app/src/main/java/com/example/healthcompass/ui/achievements/achievements_milestones_details.kt
+++ app/src/main/java/com/example/healthcompass/ui/achievements/achievements_milestones_details.kt
@@ -1,13 +1,20 @@
 package com.example.healthcompass.ui.achievements
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.healthcompass.R
+import java.io.File
+import java.io.FileOutputStream
 
 class achievements_milestones_details : Fragment() {
     private val args by navArgs<achievements_milestones_detailsArgs>()
@@ -25,6 +32,30 @@ class achievements_milestones_details : Fragment() {
         tvBadgeTitle.text = args.title
         tvBadgeDesc.text = args.desc
 
+        val badge : FrameLayout = view.findViewById(R.id.flBadge)
+
+        val btnShare : Button = view.findViewById(R.id.btnShare)
+
+        btnShare.setOnClickListener{
+
+        }
+
         return view
+    }
+
+    fun captureFrameLayout(frameLayout: FrameLayout): Bitmap {
+        frameLayout.isDrawingCacheEnabled = true
+        frameLayout.buildDrawingCache(true)
+        val bitmap = Bitmap.createBitmap(frameLayout.drawingCache)
+        frameLayout.isDrawingCacheEnabled = false
+        return bitmap
+    }
+
+    fun saveBitmapToFile(bitmap: Bitmap, context: Context): File {
+        val file = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "captured_frame.png")
+        FileOutputStream(file).use { out ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+        }
+        return file
     }
 }
